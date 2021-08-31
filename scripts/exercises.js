60d7c6cad731cdc9949c7b665e6f68d1552cb3f0
Kat Ingram <69052750+kattyeye@users.noreply.github.com>
2021-08-31 16:52:41 -0400

    add 5

--- scripts/exercises.js
+++ scripts/exercises.js
@@ -70,14 +70,25 @@ function rovarspraket(text){
   // ---------------------
   // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
   // ---------------------
-function sum(array) {
-let arrayTotal = array.length;
-let totalSum = 0;
-for (let i = 0; i < array.length; i++) {
-  totalSum += array[i];
+// function sum(array) {
+// let arrayTotal = array.length;
+// let totalSum = 0;
+// for (let i = 0; i < array.length; i++) {
+//   totalSum += array[i];
+// }
+//   return totalSum;
+// }
+// }
+
+function sum(myArray) {
+  return myArray.reduce(function(a,b) { return a + b; });
 }
-  return totalSum;
-}
+function multiply(array) {
+  let sum = 1;
+  for (var i=0; i < array.length; i++) {
+    sum = sum * array[i];
+  }
+  return sum;
 }
 
 
@@ -85,19 +96,30 @@ for (let i = 0; i < array.length; i++) {
   // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
   // ---------------------
 
-
+  function reverse(s) {
+    return [...s].reverse().join("");
+  }
 
   // ---------------------
   // Define a function findLongestWord() that takes an array of words and returns the length of the longest one.
   // ---------------------
 
-
+ function findLongestWord(str) {
+  let words = str.split('');
+  let maxLength = 0;
+  for (var i=0; i <words.length; i++) {
+    if (words[i].length > maxLength){
+      maxLength = words[i].length;
+    }
+  }
+  return maxLength;
+}
 
   // ---------------------
   // Define a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
   // ---------------------
 
-
+function filterLongWords()
 
   // ---------------------
   // Define a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
